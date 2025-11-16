c92a9edbd1d93824b6bf09ffff2b00549d8961f9
SLNR <sigurrinnan@gmail.com>
2025-11-16 10:23:27 +0100

    Improved infinite scrolling & added imgs, App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,14 @@ import {useEffect, useState, useRef} from "react";
 function App() {
     const shakeSensitivity = import.meta.env.VITE_SHAKE_SENSITIVITY;
     const fict = [
+        "60.png",
+        "7288.jpg",
+        "2805.jpeg",
+        "5451.webp",
+        "4105.jpeg",
         "672.jpeg",
         "603.jpeg",
         "3619.jpeg",
-        "2846.jpeg",
         "103.png",
         "6426.png",
         "5857.jpeg",
@@ -15,7 +19,6 @@ function App() {
         "5465.jpeg",
         "7507.png",
         "1089.jpeg",
-        "3437.jpeg",
         "6070.jpeg",
         "1349.jpeg",
         "7558.png",
@@ -30,13 +33,11 @@ function App() {
         "3167.jpeg",
         "986.jpeg",
         "2978.jpeg",
-        "123.png",
         "2051.jpeg",
         "6067.jpeg",
         "6329.jpeg",
         "3957.jpeg",
         "1382.jpeg",
-        "2214.jpeg",
         "6991.jpg",
         "7823.jpg",
         "3850.jpeg",
@@ -67,7 +68,6 @@ function App() {
         "4226.jpeg",
         "2561.jpeg",
         "5563.jpeg",
-        "7657.jpg",
     ];
     const top8 = ["7755.jpg", "1009.jpeg", "3792.jpg", "6426.png", "6310.jpeg", "4226.jpeg", "2561.jpeg", "6991.jpg"];
     const [initialArr, setInitialArr] = useState(localStorage.getItem("initArr") === "top8" ? top8 : fict);
@@ -75,19 +75,10 @@ function App() {
     const shuffle = () => {
         console.log(initialArr.length);
         if (initialArr.length > 4) {
-            const transitionImgs = JSON.parse(sessionStorage.getItem("transImgs"));
             let newArr = initialArr
             .map((value) => ({value, sort: Math.random()}))
             .sort((a, b) => a.sort - b.sort)
             .map(({value}) => value);
-            console.log(transitionImgs);
-
-            transitionImgs
-                ? transitionImgs.forEach((item) => {
-                      newArr.unshift(item);
-                  })
-                : null;
-            console.log(transitionImgs, newArr);
             return newArr;
         }
     };
@@ -124,15 +115,22 @@ function App() {
     useEffect(() => {
         window.addEventListener("devicemotion", handleMotion);
 
-        document.addEventListener("scroll", () => {
-            let height =
-                window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-            height = height * 100;
-            // console.log(height);
-            if (height + 0.3 >= 100) {
-                setImgArr(shuffle());
-            }
-        });
+        setTimeout(() => {
+            document.addEventListener("scroll", () => {
+                let height =
+                    window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+                height = height * 100;
+                // console.log(height);
+                if (height + 1 >= 100) {
+                    // setImgArr(imgArr.concat(shuffle()));
+                    const imgCont = document.querySelector("#imgContainer");
+                    const imgs = shuffle();
+                    imgs.forEach((img) => {
+                        imgCont.innerHTML += `<img src="${rootURL}/${img}" loading="lazy" />`;
+                    });
+                }
+            });
+        }, 1000);
         window.addEventListener("focus", () => {
             setHiddenState(sessionStorage.getItem("hidden") !== "false");
         });
@@ -140,12 +138,6 @@ function App() {
             setHiddenState(true);
         });
     }, []);
-
-    useEffect(() => {
-        const elem = document.querySelector("img + img");
-        elem ? elem.scrollIntoView({behavior: "auto", block: "end"}) : null;
-        scrollBy(0, -Math.abs((document.body.scrollHeight / 100) * 0.3));
-    }, [imgArr]);
 
     return (
         <>
@@ -179,16 +171,10 @@ function App() {
                             <path d="M614-634q16 17 28.5 40t19.5 48q0 23.58-16 39.79T606-490q-23 0-39.5-16.21T550-546q1-7-1-13t-7.21-11q-5.2-4-11-6.5Q525-579 518-578q-22 0-38-16t-16-38q0-22 16-38t38-16q27 6 51 19t45 33Zm-134-88q-8.89 0-17.45 1-8.55 1-17.55 1-25 2-47-11.5T369-770q-6-24 7-45t37-24q17-2.44 33.5-3.22Q463-843 480-843q148 0 272 79t188 214q6 12 9 24.44 3 12.45 3 25.5 0 13.06-1.93 26.78T942-448q-17 34-38.5 65T856-324q-17 19-41.5 17T773-328q-16-19-14.5-44t17.5-44q17-20 31-40.5t26-43.5q-53.26-99.84-146.63-160.92Q593-722 480-722Zm0 565q-145.29 0-266.15-77Q93-311 27-440q-7-14-11-28.95t-4-31q0-16.05 3-31.55T25-562q18-36 41.37-67.94Q89.74-661.87 118-691l-70-71q-15-14.73-15-35.87Q33-819 48-835q15-15 36.5-15t36.5 15l699 699q15 15 14.5 36.5T820-64q-16 15-37 15t-36-15L628-181q-35 13-72.5 18.5T480-157ZM205-608q-23 25-42.18 51.89Q143.65-529.22 128-500q50.26 101.84 144.63 161.92Q367-278 480-278q12 0 24.5-1t24.5-3l-28-31q-6 1-11 1.5t-10 .5q-79 0-134-55t-55-134q0-5-.5-10t.5-11l-86-87Zm352 69Zm-191 94Z" />
                         </svg>
                     </button>
-                    <div id="imgContainter" className={hiddenState ? "hidden" : ""}>
-                        {imgArr.map((imgSrc, index) => {
-                            if (index === imgArr.length - 1) {
-                                const transImgs = [imgArr[index], imgArr[index - 1]];
-                                console.log("transImgs", transImgs);
-
-                                sessionStorage.setItem("transImgs", JSON.stringify(transImgs));
-                            }
-                            return <img key={index} src={`${rootURL}/${imgSrc}`} loading="lazy" />;
-                        })}
+                    <div id="imgContainer" className={hiddenState ? "hidden" : ""}>
+                        {imgArr.map((imgSrc, index) => (
+                            <img key={index} src={`${rootURL}/${imgSrc}`} loading="lazy" />
+                        ))}
                     </div>
                 </>
             ) : (
